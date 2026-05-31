02b9a3f7b97f9cee183dbcd6cb329c38b5605333
Gustavo Esteves <gustavo.esteves@gmail.com>
2026-05-31 16:50:21 -0300

    chore: Clean up temporary node scratch files

--- scratch_test.js
+++
@@ -1,4 +0,0 @@
-import { Chord } from "tonal";
-
-console.log("Detecting [E, C, G]:", Chord.detect(["E", "C", "G"]));
-console.log("Detecting [C, E, G]:", Chord.detect(["C", "E", "G"]));
--- scratch_test_fields.js
+++
@@ -1,8 +0,0 @@
-import { Chord } from "tonal";
-
-const c = Chord.get("CM");
-console.log("c.root:", c.root);
-console.log("c.tonic:", c.tonic);
-console.log("c.aliases:", c.aliases);
-console.log("c.name:", c.name);
-console.log("c.notes:", c.notes);
